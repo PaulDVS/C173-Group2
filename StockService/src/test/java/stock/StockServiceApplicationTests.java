85faa7ac5c3ed70385d6d025ee2be12fd1a4a27c
Jozef <47642316+JozefBX7@users.noreply.github.com>
2022-03-16 13:35:02 +0000

    added tests to orderservice

--- StockService/src/test/java/stock/StockServiceApplicationTests.java
+++ StockService/src/test/java/stock/StockServiceApplicationTests.java
@@ -9,7 +9,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
